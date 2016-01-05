84a401c5b0842030e8882c3e2f328869c2e34b0a
Sashka Marinova <ssmarinova@gmail.com>
2016-01-05 12:55:41 +0200

    Модул 02 Видео 05 - Четене от потребителски ред

---
+++ Modul02/ReadFromCommandLine/Program.cs
@@ -0,0 +1,26 @@
+﻿using System;
+
+namespace ReadFromCommandLine
+{
+	class MainClass
+	{
+		public static void Main (string[] args)
+		{
+
+			//Дефиниране на променливи
+			int a = 6, b = 0;
+
+
+			//Въвеждане на параметри - пояснява на колко е равно a и изисква въвеждане на b
+			Console.Write ("a=" + (a)+", Моля въведете b: ");
+			b= Convert.ToInt32 (Console.ReadLine ());
+
+
+
+
+
+			//Печат на резултат
+			Console.WriteLine ( "резултата a+b е: " +(a+b).ToString()+ "\n\n\n");
+		}
+	}
+}
