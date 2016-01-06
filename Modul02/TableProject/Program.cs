f5d617e242f3d35e28dcff83c4a4c3a0a78a8afa
Sashka Marinova <ssmarinova@gmail.com>
2016-01-06 11:20:22 +0200

    Modul 02, Видео 06
    гит
    
    $
    еÑexit

--- Modul02/TableProject/Program.cs
+++ Modul02/TableProject/Program.cs
@@ -33,9 +33,27 @@ namespace TableProject
 			string list1 = string.Join (";", parse);
 			Console.WriteLine ("Новият стринг е:\n" + list1 + "\n\n");
 
+			Console.WriteLine ("\n" + "---------" + "\n");
 
 
 
+
+			//Ротативката :) от упражнението след урока ;)
+			string[] f = {"портокали","банани","лимони"};
+			var r = new Random();
+			int a1 = r.Next(0,3);
+			int a2 = r.Next(0,3);
+			int a3 = r.Next(0,3);
+			Console.WriteLine(f[a1]+" "+f[a2]+" "+f[a3]);
+
+			Console.WriteLine ("\n" + "---------" + "\n");
+
+			string [] t = {"малини","ябълки","кайсии"};
+			var c = new Random();
+			int b1 = c.Next(0,3);
+			int b2 = c.Next(0,3);
+			int b3 = c.Next(0,3);
+			Console.WriteLine (t[b1]+" "+t[b2]+" "+t[b3]);
 
 		}
 	}
