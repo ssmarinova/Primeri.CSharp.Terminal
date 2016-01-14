d029d6421998bb14ff6556d1b88c7160352f181f
Sashka Marinova <ssmarinova@gmail.com>
2016-01-14 09:26:45 +0200

    М03, В03 - работа с променливи - продълж.

--- Modul03/Modul03/Program.cs
+++ Modul03/Modul03/Program.cs
@@ -13,12 +13,26 @@ namespace Modul03
 			Console.Write ("> a = ");	a = Convert.ToDouble (Console.ReadLine ());
 			Console.Write ("> b = ");	b = Convert.ToDouble (Console.ReadLine ());
 
+			//Форматиране с дименсия
 			string numberFormat = "#0.00 m3";
 			Console.WriteLine ("\n\n>a / b = "+ (a/b).ToString(numberFormat));
 
-		
+			//Степенуване
+			c=Math.Pow(a,b);
+			Console.WriteLine ("\n\n>a ^ b = "+ c.ToString(numberFormat));
 
 		
+			//Коренуване
+			c=Math.Sqrt(b);
+			Console.WriteLine ("\n\n>b ^ 1/2 = "+ c.ToString(numberFormat));
+
+
+			//Закръгления
+			c=Math.Round(a/b);
+			Console.WriteLine ("\n\n>a / b = "+ c.ToString());
+
+
+
 		}
 	}
 }
