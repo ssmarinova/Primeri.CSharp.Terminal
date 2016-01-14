d85c9cd6aaf5c9bf3fd91f78c636012a74845c98
Sashka Marinova <ssmarinova@gmail.com>
2016-01-14 10:55:35 +0200

    М03, В06 - Работа с условни оператори - част 1

--- Modul03/BoolExplore/Program.cs
+++ Modul03/BoolExplore/Program.cs
@@ -15,29 +15,42 @@ namespace BoolExplore
 
 			int _ui = Convert.ToInt32 (Console.ReadLine () );
 
-			bool _check01 = _ui <= _i.Length;
-			bool _check02 = _ui > 0;
+//			bool _check01 = _ui <= _i.Length;
+//			bool _check02 = _ui > 0;
+//
+//			bool _exp01 = _check01 &&	_check02;     //Лофическо и "&&".
+//			bool _exp02 = _check01 ||	_check02;     //Лофическо или "||".
+//			bool _exp03 = !_check01;     //Лофическо отрицание "!".
+//
+//
+//
+////			Console.WriteLine (_test);
+////			Console.WriteLine (_b);
+////			Console.WriteLine ();
+//
+////			Console.WriteLine (_check01);
+////			Console.WriteLine (_check02);
+////			Console.WriteLine ( _i [_ui-1] );
+//
+//			Console.WriteLine ("Логическо и: " + _exp01.ToString () );
+//			Console.WriteLine ("Логическо или: " + _exp02.ToString () );
+//			Console.WriteLine ("Отрицание на: " + _check01.ToString () + ": " + _exp03);
 
-			bool _exp01 = _check01 &&	_check02;     //Лофическо и "&&".
-			bool _exp02 = _check01 ||	_check02;     //Лофическо или "||".
-			bool _exp03 = !_check01;     //Лофическо отрицание "!".
+
+			if (_ui > 0 && _ui <= _i.Length) {
+				Console.WriteLine (_i [_ui - 1]);
+			} else {
+				if (_ui > _i.Length) {
+					Console.WriteLine ("Индекса е прекалено голям.\n\n\n");
+				}
+				if (_ui <= 0) {
+					Console.WriteLine ("Моля въведете число по-голямо от 0.\n\n\n");	
+				}
+			  }
 
 
 
-//			Console.WriteLine (_test);
-//			Console.WriteLine (_b);
-//			Console.WriteLine ();
 
-//			Console.WriteLine (_check01);
-//			Console.WriteLine (_check02);
-//			Console.WriteLine ( _i [_ui-1] );
-
-			Console.WriteLine ("Логическо и: " + _exp01.ToString () );
-			Console.WriteLine ("Логическо или: " + _exp02.ToString () );
-			Console.WriteLine ("Отрицание на: " + _check01.ToString () + ": " + _exp03);
-
-
-
-		}
+		}  //kraj Metod
 	}
 }
