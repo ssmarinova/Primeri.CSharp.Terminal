49add96692b133b0e99579ba2ff27749c7aca4eb
Sashka Marinova <ssmarinova@gmail.com>
2016-01-14 14:27:12 +0200

    М03, В05, - работа с bool- Ñ7аст 03 и условни оператори

--- Modul03/BoolExplore/Program.cs
+++ Modul03/BoolExplore/Program.cs
@@ -7,48 +7,70 @@ namespace BoolExplore
 		public static void Main (string[] args)
 		{
 
-			bool _b = true;
-
-			bool _test = 5 > 10;
+//			bool _b = true;
+//
+//			bool _test = 5 > 10;
 
 			int[] _i = new int[] { 4, 3, 2 };
 
-			int _ui = Convert.ToInt32 (Console.ReadLine () );
+			int _ui = 0;  //Convert.ToInt32 (Console.ReadLine () );
 
-//			bool _check01 = _ui <= _i.Length;
-//			bool _check02 = _ui > 0;
-//
-//			bool _exp01 = _check01 &&	_check02;     //Лофическо и "&&".
-//			bool _exp02 = _check01 ||	_check02;     //Лофическо или "||".
-//			bool _exp03 = !_check01;     //Лофическо отрицание "!".
-//
-//
-//
-////			Console.WriteLine (_test);
-////			Console.WriteLine (_b);
-////			Console.WriteLine ();
-//
-////			Console.WriteLine (_check01);
-////			Console.WriteLine (_check02);
-////			Console.WriteLine ( _i [_ui-1] );
-//
-//			Console.WriteLine ("Логическо и: " + _exp01.ToString () );
-//			Console.WriteLine ("Логическо или: " + _exp02.ToString () );
-//			Console.WriteLine ("Отрицание на: " + _check01.ToString () + ": " + _exp03);
+			string _input = "0";
+			Console.Write ("Моля въведете индекс: ");
+			_input = Console.ReadLine ();
+
+			//Проверка дали въведеното от потребителя е число
+			bool _check01 = int.TryParse (_input, out _ui); //int.TryParse конвертира текст в тип int, като връща резултат дали се е справил или - не. 
+			                                                //Kомандата означава: превърни _input (което е string) в променливата _ui (която е int).
 
 
-			if (_ui > 0 && _ui <= _i.Length) {
+			//Проверка на горната граница
+			bool _check02 = _ui <= _i.Length;
+
+
+			//Проверка на долната граница
+			bool _check03 = _ui > 0;
+
+
+
+
+			if (_check01 && _check02 && _check03) {
 				Console.WriteLine (_i [_ui - 1]);
-			} else {
-				if (_ui > _i.Length) {
-					Console.WriteLine ("Индекса е прекалено голям.\n\n\n");
-				}
-				if (_ui <= 0) {
-					Console.WriteLine ("Моля въведете число по-голямо от 0.\n\n\n");	
-				}
-			  }
+			} 
+
+			if (!_check01) {
+				Console.WriteLine ("Индекс трябва да бъде цяло число.\n\n\n");
+			}
+			if (!_check02) {
+				Console.WriteLine ("Индекса е прекалено голям.\n\n\n");
+			}
+			if (!_check03 && _check01) {
+				Console.WriteLine ("Моля въведете число по-голямо от 0.\n\n\n");	
+			}
+			  
 
 
+
+			//			bool _check01 = _ui <= _i.Length;
+			//			bool _check02 = _ui > 0;
+			//
+			//			bool _exp01 = _check01 &&	_check02;     //Лофическо и "&&".
+			//			bool _exp02 = _check01 ||	_check02;     //Лофическо или "||".
+			//			bool _exp03 = !_check01;     //Лофическо отрицание "!".
+			//
+			//
+			//
+			////			Console.WriteLine (_test);
+			////			Console.WriteLine (_b);
+			////			Console.WriteLine ();
+			//
+			////			Console.WriteLine (_check01);
+			////			Console.WriteLine (_check02);
+			////			Console.WriteLine ( _i [_ui-1] );
+			//
+			//			Console.WriteLine ("Логическо и: " + _exp01.ToString () );
+			//			Console.WriteLine ("Логическо или: " + _exp02.ToString () );
+			//			Console.WriteLine ("Отрицание на: " + _check01.ToString () + ": " + _exp03);
 
 
 		}  //kraj Metod
---
+++ Modul03/Uprajnenia/Program.cs
@@ -0,0 +1,48 @@
+﻿using System;
+
+namespace Uprajnenia
+{
+	class MainClass
+	{
+		public static void Main (string[] args)
+		{
+
+
+			//Rotativka
+			string[] row = {"портокали","банани","лимони"};
+			var r = new Random();
+			int[] a = new int[3] {r.Next(0,3), r.Next(0,3), r.Next(0,3)};
+
+
+			Console.WriteLine(row[a[0]]+" "+row[a[1]]+" "+row[a[2]]);
+
+			int pechalba = 0;
+
+			// Три еднакви
+			if ( (a[0] == a[1]) && (a[0] == a[2]) ) pechalba += 5;
+
+			// Две еднакви
+			if ( (a[0] == a[1]) || (a[0] == a[2]) || (a[1] == a[2]) ) pechalba += 5;
+
+			Console.WriteLine("Печелите "+pechalba+" точки");
+
+
+
+			//Ротативка - втори вариант за същото
+			var I = new Random();
+			int[] l = new int[4] {I.Next(0,3), I.Next(0,3), I.Next(0,3),0};
+			if ( (l[0] == l[1]) && (l[0] == l[2]) ) l[3] = 10;
+			else if ( (l[0] == l[1]) || (l[0] == l[2]) || (l[1] == l[2]) ) l[3] = 5;
+			string[] row1 = {"портокали","банани","лимони"};
+			Console.WriteLine(row1[l[0]]+" "+row1[l[1]]+" "+row1[l[2]]+"\nПечелите "+l[3]+" точки");
+
+
+
+
+
+
+
+
+		}
+	}
+}
