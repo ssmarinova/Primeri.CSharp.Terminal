f3eaf36343a4df0921f29af90c48bf8e39333d41
Sashka Marinova <ssmarinova@gmail.com>
2016-01-14 15:22:21 +0200

    М03, В09 - Използване на Switch

--- Modul03/BoolExplore2/Program.cs
+++ Modul03/BoolExplore2/Program.cs
@@ -19,12 +19,44 @@ namespace BoolExplore2
 			bool _check01 = int.TryParse (_input, out _ui); 
 
 			//ако потребителя въведе по-голямо число, присвоява последното, ако въвде по-малко - присвоява първото - без да дава индикации 
-			_ui	= (_ui <= _i.Length) ? _ui : 3; //Ако _ui <= _i.Length, да се запази _ui, иначе да стане 2 
-			_ui	= (_ui > 0) ? _ui : 1;
+//			_ui	= (_ui <= _i.Length) ? _ui : 3; //Ако _ui <= _i.Length, да се запази _ui, иначе да стане 3 
+//			_ui	= (_ui > 0) ? _ui : 1;
 
-			if (_check01) {
-				Console.WriteLine (_i [_ui - 1]);
-			} 
+			switch (_ui) {
+			case 1:
+				{ 
+					Console.Write ("Първи елемент от масива: ");
+					Console.WriteLine (_i [_ui-1]);
+					break;
+				}
+			case 2:
+				{ 
+					Console.Write ("Втори елемент от масива: ");
+					Console.WriteLine (_i [_ui-1]);
+					break;
+				}
+			case 3:
+				{ 
+					Console.Write ("Трети елемент от масива: ");
+					Console.WriteLine (_i [_ui-1]);
+					break;
+				}
+			default: 
+				{
+					Console.Write ("В масива няма стойност за този индекс. \nОпитайте с индекс между 1 и 3 \n\n");
+					break;
+				}
+			}
+
+
+
+
+
+
+
+//			if (_check01) {
+//				Console.WriteLine (_i [_ui - 1]);
+//			} 
 
 			//  !!не дава грешка ако се въведе буква или друг символ 
 
