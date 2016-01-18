2dc6479747af9277870b2b85fc77ec7f8c4bfd9d
Sashka Marinova <ssmarinova@gmail.com>
2016-01-18 10:30:25 +0200

    М04, В06 - Пример с do..while до задаване на изход от потребителя

---
+++ Modul04/ListExplore/Program.cs
@@ -0,0 +1,28 @@
+﻿using System;
+
+namespace ListExplore
+{
+	class MainClass
+	{
+		public static void Main (string[] args)
+		{
+
+			// Дефиниции
+			string usimp = "";
+
+
+			//Потрбителски вход
+			do {
+				Console.Write ("? ");
+				usimp = Console.ReadLine ();
+
+
+			} while(usimp != "exit");
+
+
+
+
+
+		}//krai metod
+	}
+}
