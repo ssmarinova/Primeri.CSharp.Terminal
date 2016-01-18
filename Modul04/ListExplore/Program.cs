5a45d312c28e8130f258468b7a13ead60818d80a
Sashka Marinova <ssmarinova@gmail.com>
2016-01-18 10:47:41 +0200

    М04, В07 - Работа с List - преглед и дефиниране

--- Modul04/ListExplore/Program.cs
+++ Modul04/ListExplore/Program.cs
@@ -1,4 +1,5 @@
 ﻿using System;
+using System.Collections.Generic;
 
 namespace ListExplore
 {
@@ -9,12 +10,37 @@ namespace ListExplore
 
 			// Дефиниции
 			string usimp = "";
+			// Дефиниране на лист = като масив, но е по-динамичен - може да добавяме и трием елементи без проблем.
+			List<int> mylist1 = new List<int> ();
+
+
+			mylist1.Add (5);
+			mylist1.Add (3);
+			mylist1.Add (9);
 
 
 			//Потрбителски вход
 			do {
 				Console.Write ("? ");
 				usimp = Console.ReadLine ();
+
+				// Добавяне на стойности
+
+
+				//Преглед на листа
+				if( usimp.Contains("show") ) {
+					Console.Write ( "Съдържанието на листа е: ");
+					for (int i=0; i<mylist1.Count; i++) {
+						Console.Write (mylist1 [i]);
+
+						if ( i != mylist1.Count -1) Console.Write (", ");
+					}
+					Console.WriteLine ();
+				}
+
+				//размер на листа
+
+
 
 
 			} while(usimp != "exit");
